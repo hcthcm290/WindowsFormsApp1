e154665586099459a24a618a27af79039dffd099
dragonnica123 <dragonnica123@gmail.com>
2019-09-21 08:45:58 +0700

    add ability to control calendar view

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,36 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.day = new System.Windows.Forms.NumericUpDown();
+            this.startHour = new System.Windows.Forms.NumericUpDown();
+            this.startMinute = new System.Windows.Forms.NumericUpDown();
+            this.endHour = new System.Windows.Forms.NumericUpDown();
+            this.endMinute = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.day)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endMinute)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // table
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(102, 99);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(664, 305);
-            this.dataGridView1.TabIndex = 0;
+            this.table.AutoSize = true;
+            this.table.BackColor = System.Drawing.SystemColors.Info;
+            this.table.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.table.ColumnCount = 5;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.table.Location = new System.Drawing.Point(34, 27);
+            this.table.Name = "table";
+            this.table.RowCount = 5;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.table.Size = new System.Drawing.Size(506, 250);
+            this.table.TabIndex = 0;
+            this.table.Paint += new System.Windows.Forms.PaintEventHandler(this.table_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // day
+            // 
+            this.day.Location = new System.Drawing.Point(546, 27);
+            this.day.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(73, 20);
+            this.day.TabIndex = 3;
+            this.day.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // startHour
+            // 
+            this.startHour.Location = new System.Drawing.Point(546, 76);
+            this.startHour.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.startHour.Name = "startHour";
+            this.startHour.Size = new System.Drawing.Size(73, 20);
+            this.startHour.TabIndex = 4;
+            // 
+            // startMinute
+            // 
+            this.startMinute.Location = new System.Drawing.Point(681, 76);
+            this.startMinute.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.startMinute.Name = "startMinute";
+            this.startMinute.Size = new System.Drawing.Size(73, 20);
+            this.startMinute.TabIndex = 5;
+            // 
+            // endHour
+            // 
+            this.endHour.Location = new System.Drawing.Point(546, 131);
+            this.endHour.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.endHour.Name = "endHour";
+            this.endHour.Size = new System.Drawing.Size(73, 20);
+            this.endHour.TabIndex = 6;
+            // 
+            // endMinute
+            // 
+            this.endMinute.Location = new System.Drawing.Point(681, 131);
+            this.endMinute.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.endMinute.Name = "endMinute";
+            this.endMinute.Size = new System.Drawing.Size(73, 20);
+            this.endMinute.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(784, 421);
+            this.Controls.Add(this.endMinute);
+            this.Controls.Add(this.endHour);
+            this.Controls.Add(this.startMinute);
+            this.Controls.Add(this.startHour);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.table);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.day)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.startMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endMinute)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TableLayoutPanel table;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown day;
+        private System.Windows.Forms.NumericUpDown startHour;
+        private System.Windows.Forms.NumericUpDown startMinute;
+        private System.Windows.Forms.NumericUpDown endHour;
+        private System.Windows.Forms.NumericUpDown endMinute;
+
+        private void add()
+        {
+            System.Windows.Forms.Button h = new System.Windows.Forms.Button();
+            h.Name = "U";
+            this.Controls.Add(h);
+        }
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,14 +17,63 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int d;
+            int startH, startM;
+            int endH, endM;
+            int widthT, heightT;
+            int offset = 5;
+            
+            d = (int)day.Value;
+            
+            startH = (int)startHour.Value;
+            startM = (int)startMinute.Value;
+            
+            endH = (int)endHour.Value;
+            endM = (int)endMinute.Value;
+            
+            widthT = table.Width / table.ColumnCount;
+            heightT = table.Height / table.RowCount;
+            
+            Point position = new Point();
+            position.X = d * widthT + offset + table.Location.X;
+            position.Y = (int)(heightT * (startH + (float)startM / 60)) + table.Location.Y;
+
+            int positionEnd_Y;
+            positionEnd_Y = (int)(heightT * (endH + (float)endM / 60)) + table.Location.Y;
+
+            Panel h = new Panel();
+            h.BackColor = Color.FromArgb(100, 255, 0, 0);
+            h.Location = position;
+            h.Width = widthT - 2 * offset;
+            h.Height = positionEnd_Y - position.Y;
+            h.Location = position;
+            h.Name = "thanh";
+            
+
+            
+            this.Controls.Add(h);
+
+            h.BringToFront();
+            
+
+            //add();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        private void table_Paint(object sender, PaintEventArgs e)
         {
-            // this is a test
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
